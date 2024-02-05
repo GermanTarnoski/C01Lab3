a68d1bbaf4162e10683a1928967e11ec0af7392a
germantarnoski <germantarnoski16@gmail.com>
2024-02-05 01:18:48 -0300

    lab3

---
+++ quirknotes/frontend/src/App.js
@@ -0,0 +1,199 @@
+import React, {useState, useEffect} from "react"
+import './App.css';
+import Dialog from "./Dialog";
+import Note from "./Note";
+
+function App() {
+
+  // -- Backend-related state --
+  const [loading, setLoading] = useState(true)
+  const [notes, setNotes] = useState(undefined)
+
+  // -- Dialog props-- 
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [dialogNote, setDialogNote] = useState(null)
+
+  
+  // -- Database interaction functions --
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        await fetch("http://localhost:4000/getAllNotes")
+        .then(async (response) => {
+          if (!response.ok) {
+            console.log("Served failed:", response.status)
+          } else {
+              await response.json().then((data) => {
+              getNoteState(data.response)
+          }) 
+          }
+        })
+      } catch (error) {
+        console.log("Fetch function failed:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    getNotes()
+  }, [])
+
+const deleteNote = async (entry) => {
+    // Code for DELETE note here
+      try {
+      const response = await fetch(`http://localhost:4000/deleteNote/${entry._id}`,
+      {
+        method: "DELETE"
+      });
+
+      if (!response.ok) {
+        console.log("Error deleting note:", response.status);
+        alert("Error deleting note. Please try again.");
+      }
+    } catch (error) {
+        console.log("Fetch function failed:", error)
+      } finally {
+        setNotes(deleteNoteState(entry))
+      }
+    }
+
+
+  const deleteAllNotes = async (notes) => {
+    // Code for DELETE all notes here
+    try {
+      const response = await fetch(`http://localhost:4000/deleteAllNotes`,
+      {
+        method: "DELETE"
+      });
+     if (!response.ok) {
+        console.log("Error deleting notes:", response.status);
+        alert("Error deleting notes. Please try again.");
+      }
+    } catch (error) {
+        console.log("Fetch function failed:", error)
+      } finally {
+        setNotes(deleteAllNotesState(notes))
+      }
+    }
+
+  
+  // -- Dialog functions --
+  const editNote = (entry) => {
+    setDialogNote(entry)
+    setDialogOpen(true)
+  }
+
+  const postNote = () => {
+    setDialogNote(null)
+    setDialogOpen(true)
+  }
+
+  const closeDialog = () => {
+    setDialogNote(null)
+    setDialogOpen(false)
+  }
+
+  // -- State modification functions -- 
+  const getNoteState = (data) => {
+    setNotes(data)
+  }
+
+  const postNoteState = (_id, title, content) => {
+    setNotes((prevNotes) => [...prevNotes, {_id, title, content}])
+  }
+
+  const deleteNoteState = (entry) => {
+    // Code for modifying state after DELETE here
+    return notes.filter((note) => note._id !== entry._id) // this returns a new array with the deleted note removed.
+    // the new array is created by adding all notes that don't match the deleted note.
+  }
+
+  const deleteAllNotesState = () => {
+      return [] // we can remove all the elements of the array, or we can just return an empty array.
+  }
+
+  const patchNoteState = (_id, title, content) => {
+    // Code for modifying state after PATCH here
+    const newNotes = notes.map((note) => {
+      if (note._id === _id) {
+        note.title = title
+        note.content = content
+      }
+      return note
+    })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={dialogOpen ? AppStyle.dimBackground : {}}>
+          <h1 style={AppStyle.title}>QuirkNotes</h1>
+          <h4 style={AppStyle.text}>The best note-taking app ever </h4>
+
+          <div style={AppStyle.notesSection}>
+            {loading ?
+            <>Loading...</>
+            : 
+            notes ?
+            notes.map((entry) => {
+              return (
+              <div key={entry._id}>
+                <Note
+                entry={entry} 
+                editNote={editNote} 
+                deleteNote={deleteNote}
+                />
+              </div>
+              )
+            })
+            :
+            <div style={AppStyle.notesError}>
+              Something has gone horribly wrong!
+              We can't get the notes!
+            </div>
+            }
+          </div>
+
+          <button onClick={postNote}>Post Note</button>
+          {notes && notes.length > 0 && 
+          <button
+              onClick={deleteAllNotes}
+              >
+              Delete All Notes
+          </button>}
+
+        </div>
+
+        <Dialog
+          open={dialogOpen}
+          initialNote={dialogNote}
+          closeDialog={closeDialog}
+          postNote={postNoteState}
+          patchNote={patchNoteState}
+          />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+const AppStyle = {
+  dimBackground: {
+    opacity: "20%", 
+    pointerEvents: "none"
+  },
+  notesSection: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: "center"
+  },
+  notesError: {color: "red"},
+  title: {
+    margin: "0px"
+  }, 
+  text: {
+    margin: "0px"
+  }
+}
